3668d6a7b30d4d1d383e205074a6adf563f0bae8
pythonstudy2024 <pytonstudy2024@gmail.com>
2024-06-18 23:47:48 +0900

    settings.py update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ozvx4huk1^%$o6q=w=7e1&64n_8-sb8hv_sgpx_=%l=a4riu92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
